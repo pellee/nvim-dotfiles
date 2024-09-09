e3c7a2245fa6cfcc71a0ef4da027e4d41d55ca18
pellee <joaquinpellegrino@gmail.com>
2024-09-09 19:00:23 -0300

    auto indent .lua file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,11 +4,11 @@ return {
   config = function ()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-              ensure_installed = { "lua", "markdown", "markdown_inline", "c", "cpp", "vimdoc" }, -- what parsers we want ensured are installed in tree-sitter.
-              sync_install = false,
-              highlight = { enable = true }, -- highlitghts on the languages installed.
-              indent = { enable = true }, -- same as before. indent on the languages installed. 
-          })
+      ensure_installed = { "lua", "markdown", "markdown_inline", "c", "cpp", "vimdoc" }, -- what parsers we want ensured are installed in tree-sitter.
+      sync_install = false,
+      highlight = { enable = true }, -- highlitghts on the languages installed.
+      indent = { enable = true }, -- same as before. indent on the languages installed. 
+    })
   end
 
 }
