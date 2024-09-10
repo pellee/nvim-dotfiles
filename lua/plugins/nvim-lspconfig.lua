4e1dbf159d428348e8b72331bf030480241faea1
pellee <joaquinpellegrino@gmail.com>
2024-09-09 22:59:34 -0300

    Add lsp, formatter and treesitter for nix files.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,6 +4,7 @@ return {
     local lspConfig = require("lspconfig")
     -- here i have to set up the lsp that were instaled with mason-lspconfig
     lspConfig.lua_ls.setup({})
+    lspConfig.nixd.setup({})
     vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
